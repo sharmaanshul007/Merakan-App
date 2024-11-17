21096e6d3491c030e21ec0b8eeca5a0637075a71
sharmaanshul007 <sasansanshul@gmail.com>
2024-11-17 19:20:36 +0530

    added the functionality to select and remove a category

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import {Sidebar} from 'flowbite-react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addCategory, removeCategory } from '../redux/slices/selectedCategory.jsx';
 const Sidebarcategory = ({allCategories}) => {
-  
+  const {selectedCategories} = useSelector((state)=>state.category);
+  const dispatch = useDispatch();
+  const handleCategoryChange = (e) => {
+    const newCategory = e.target.name;
+    selectedCategories.includes(newCategory) 
+    ? 
+    (dispatch(removeCategory(newCategory))) 
+    : 
+    (dispatch(addCategory(newCategory)))
+  }
+  console.log(selectedCategories);
   return (
     <div className='max-w-[200px] flex flex-col flex-wrap w-[300px] items-center justify-center'>
         <h3 className='font-semibold text-gray-400 text-[20px] mb-3'>Categories</h3>
         {
             allCategories.map((category,index) => (
-                <div key={index} className='flex gap-2 items-center'>
+                <div key={index} className='flex gap-2 items-center' onClick={handleCategoryChange}>
                     <label htmlFor={`category-${index}`}>{category}</label>
-                    <input id={`category-${index}`} type='checkbox'>
+                    <input name={category} id={`category-${index}`} type='checkbox'>
                     </input>
                 </div>
             ))
