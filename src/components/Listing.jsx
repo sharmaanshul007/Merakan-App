a1940d9dd19e1f60f544d59cb83593cdac2b0431
sharmaanshul007 <sasansanshul@gmail.com>
2024-11-18 00:13:13 +0530

    Finally co-ordinated the updation of category and products together

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ProductItem from './ProductItem'
 const Listing = ({products}) => {
   return (
-    <div>
+    <div className='grid grid-cols-1 w-[90%] mx-auto text-center mt-7 sm:grid-cols-2 md:grid-cols-3 gap-y-5 gap-x-2 p-3'>
       {
         products.map((product,index)=>{return <ProductItem key={index}product={product}></ProductItem>})
       }
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
 const ProductItem = ({product}) => {
-    const description = product;
+    const smallDescription = product.description.split(' ').slice(0,20).join(' ') + "...";
   return (
-    <div>
-      {product.category}
+    <div className='flex flex-col gap-y-2 border rounded-lg justify-center items-center'>
+      <span className='font-semibold font-serif mt-1'>{product.name}</span>
+      <div className="w-[19vw] h-[30vh] overflow-hidden">
+            <img
+            className="w-full h-full object-cover rounded-lg"
+            src={product.image}
+            alt={product.name}
+            />
+        </div>
+      <span className='font-sans'>$ {product.price}</span>
+      <span className='font-serif mb-4 p-2'>{smallDescription}</span>
     </div>
   )
 }
