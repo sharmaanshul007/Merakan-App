8d0621733baca694e9a74c8d8328a2fca1f0f6a7
sharmaanshul007 <sasansanshul@gmail.com>
2024-11-17 14:50:12 +0530

    Added two Routes, one for Home page and second one for specific product page

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{html,js,jsx,ts,tsx}',  // Adjust based on your project structure
+    './public/index.html',              // If you have HTML in the public folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
